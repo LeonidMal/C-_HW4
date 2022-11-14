6b1aad468aed85ef7dd4251048a04eb0a45e2dd1
Leonid <rockwil91@gmail.com>
2022-11-14 19:34:27 +0200

    Д/З 4 - Задача 29

---
+++ Task 1/Program.cs
@@ -0,0 +1,59 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+/*Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number > 0)
+{
+
+}
+*/
+
+
+
+
+
+
+Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+
+int numLength = number.Length;
+int[] sum = new int[numLength];
+//int summary = 0;
+
+for (int i = 0; i < numLength; i++)
+{
+    string digit = number[i].ToString();
+    sum[i] = Convert.ToInt32(number[i]);
+    /*for (int j = 0; j < sumLength; j++)
+    {     
+        summary = summary + sum[i];
+    }  */
+}
+
+Console.WriteLine(sum[2]);
+
+/*Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+
+int Sum (string number, int digit)
+{
+    int result = 0;
+
+    int length = number.Length;
+    for (int i = 0; i < length; i++)
+    {
+        digit = Convert.ToInt32(number[i]);
+        result = result + digit;
+    }
+    return result;
+}
+
+int sum = Sum(number, 0);
+Console.WriteLine($"{number} -> {sum}");*/
---
+++ Task 2/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+
+Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [value];
+
+for (int i = 0; i < value; i++)
+{
+    Console.Write($"{array[i] = new Random().Next(1, 1000)} ");
+} 
