0f22fbc344dcfc1634b1256235348bb866f2a4d1
Leonid <rockwil91@gmail.com>
2022-11-15 19:26:24 +0200

    Д/З 4 - Задача 27

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -6,54 +6,19 @@
 
 9012 -> 12*/
 
-/*Console.WriteLine("Введите число ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-while (number > 0)
-{
-
-}
-*/
-
-
-
-
-
-
 Console.WriteLine("Введите число ");
 string number = Console.ReadLine();
 
 int numLength = number.Length;
 int[] sum = new int[numLength];
-//int summary = 0;
+int summary = 0;
 
 for (int i = 0; i < numLength; i++)
 {
     string digit = number[i].ToString();
-    sum[i] = Convert.ToInt32(number[i]);
-    /*for (int j = 0; j < sumLength; j++)
-    {     
-        summary = summary + sum[i];
-    }  */
+    sum[i] = Convert.ToInt32(digit);
+    summary = summary + sum[i];
+    
 }
 
-Console.WriteLine(sum[2]);
-
-/*Console.WriteLine("Введите число ");
-string number = Console.ReadLine();
-
-int Sum (string number, int digit)
-{
-    int result = 0;
-
-    int length = number.Length;
-    for (int i = 0; i < length; i++)
-    {
-        digit = Convert.ToInt32(number[i]);
-        result = result + digit;
-    }
-    return result;
-}
-
-int sum = Sum(number, 0);
-Console.WriteLine($"{number} -> {sum}");*/
+Console.WriteLine(summary);
